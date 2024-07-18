f482077f63410fdd4795ffc4b54e4dacd0a3a6b7
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-18 17:20:03 +0300

    Add project files.

---
+++ problem13/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem13
+{
+    class BMICalculator
+    {
+        public Stack<double> bmiHistory = new Stack<double>();
+
+        public double CalculateBMI(double height, double weight)
+        {
+            double bmi = weight / (height * height);
+            AddBMIHistory(bmi);
+            return bmi;
+        }
+        
+        
+        public string GetBMICategory(double bmi)
+        {
+            AddBMIHistory(bmi);
+            if (bmi < 18.5)
+                return "Underweight";
+            else if (bmi < 25)
+                return "Normal Weight";
+            else if (bmi < 30)
+                return "Overweight";
+            else
+                return "Obesity";
+        }
+
+        public void AddBMIHistory(double bmi)
+        {
+            bmiHistory.Push(bmi);
+        }
+        public void show_history(double height, double weight, double bmi)
+        {
+            Console.WriteLine($"""
+                height : {height} m
+                width : {weight} m
+                bmi : {bmi} m^-2 
+                """);
+        }
+    }
+}
---
+++ problem13/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace problem13
+{
+    internal class Program
+    {
+
+            static void Main(string[] args)
+            {
+                BMICalculator bmiCalculator = new BMICalculator();
+
+
+                Console.WriteLine("Enter your height (in meters):");
+                double height = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter your weight (in kilograms):");
+                double weight = double.Parse(Console.ReadLine());
+
+
+                double bmi = bmiCalculator.CalculateBMI(height, weight);
+
+
+                Console.WriteLine($"Your BMI is: {bmi}");
+                Console.WriteLine($"BMI Category: {bmiCalculator.GetBMICategory(bmi)}");
+
+
+                bmiCalculator.AddBMIHistory(bmi);
+                bmiCalculator.show_history(height, weight, bmi);
+                Console.ReadLine();
+            }
+        
+    }
+}
